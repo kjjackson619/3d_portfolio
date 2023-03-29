adf83efef806a18e340cff4bec1ebcc2037f683c
KJ Jackson <kjjackson619@yahoo.com>
2023-03-28 22:38:34 -0400

    email functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
 import { slideIn } from '../utils/motion';
 
+
 const Contact = () => {
 
   const formRef = useRef();
@@ -18,9 +19,43 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => { }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => { }
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    emailjs.send(
+      'service_4f038va',
+      'template_qg5679s',
+      {
+        from_name: form.name,
+        to_name: 'Kevin',
+        from_email: form.email,
+        to_email: 'kevin.jsdeveloper@gmail.com',
+        message: form.message,
+      },
+      'EAdAKkB2EJ9B_5dpn'
+    ).then(() => {
+      setLoading(false);
+      alert("Thank you. I will get back to you as soon as possible.");
+      setForm({
+        name: "",
+        email: "",
+        message: "",
+      })
+    }, (error) => {
+      setLoading(false)
+      console.log(error);
+      alert("Something went wrong.")
+    })
+
+  }
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
