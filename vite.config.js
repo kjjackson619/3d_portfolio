18eb99b49ceed9608fa959c1a9dc76dfb143a507
KJ Jackson <kjjackson619@yahoo.com>
2023-05-15 17:54:07 -0400

    debug

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
 import { defineConfig } from 'vite'
 import React from 'react';
-import {resolve} from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [React()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/main.jsx'),
-    },
-    rollupOptions: {
-      output: {
-        "format": "esm",
-        "type": "application/javascript",
-      }
-    }
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx',
-      '.json',
-    ],
-  },
-  base: './dist',
 })
