9251d05d1ff166cd65380d12f27b2d4271aecd69
KJ Jackson <kjjackson619@yahoo.com>
2023-05-19 13:15:33 -0400

    debug

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}", "!./src/main.jsx"],
+  content: ["./src/**/*.{js,mjs}"],
   mode: "jit",
   theme: {
     extend: {
