00bd1d5355d5a5530ca7e27c5c980484e4adf76b
KJ Jackson <kjjackson619@yahoo.com>
2023-05-22 21:00:08 -0400

    pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/3d_portfolio/',
+  base: '/',
   plugins: [react()],
   build: {
+    target: 'modules',
     chunkSizeWarningLimit: 1000,
   }
 });
